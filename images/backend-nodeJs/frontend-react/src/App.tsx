016cf097085b2119edcd280e40d80191f89c3935
niko.brusselaers <niko.brusselaers@outlook.com>
2023-12-11 12:54:24 +0100

    feat: created form with fetch request
    created form and fetch request to fetch data from backend

--- images/backend-nodeJs/frontend-react/src/App.tsx
+++ images/backend-nodeJs/frontend-react/src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const API_URL = 'http://localhost:4000'
+
 function App() {
+  const [webDomain, setWebDomain] = useState<string>('torfs.be')
+  const [webURL, setWebURL] = useState<string>('')
+
+  const handleForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios.post(`${API_URL}/getPageData`, {
+      websiteDomain: webDomain,
+      url: webURL
+    })
+    .then((data) => {
+      console.log(data)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleForm}>
+        <label htmlFor="webDomain">please select web domain to scrap</label>
+        <select name="webDomain" id="Webdomain" onChange={(event) => {setWebDomain(event.currentTarget.value)}}>
+          <option value="torfs.be">torfs.be</option>
+          <option value="bol.com">bol.com</option>
+          <option value="gamme.be">gamma.be</option>
+          <option value="dreamland.be">dreamland.be</option>
+        </select>
+        <label htmlFor="webURL">please enter url to scrape</label>
+        <input type="text" name="webURL" id="webURL" onChange={(event)=> {setWebURL(event.currentTarget.value)}}/>
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
